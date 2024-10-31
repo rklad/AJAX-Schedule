3ad4bb7d2d8f55c4ca774894ae127accff5c74ba
rklad <525RKladerman@frhsd.com>
2024-10-31 13:46:31 -0400

    appended schedule and got it to remove the previous day's schedule, need to work on order of classes and time

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ $(document).ready(function () {
         1: { start: "8:24 AM", end: "9:31 AM" },
         2: { start: "9:36 AM", end: "10:48 AM" },
         3: { start: "10:53 AM", end: "11:55 AM" },
-        4: { start: "12:00 PM", end: "12:35 PM" },
-        5: { start: "12:41 PM", end: "1:48 PM" },
-        6: { start: "1:53 PM", end: "3:00 PM" }
+        Lunch: { start: "12:00 PM", end: "12:35 PM" },
+        4: { start: "12:41 PM", end: "1:48 PM" },
+        5: { start: "1:53 PM", end: "3:00 PM" }
     };
+
+    const periods = {
+        A: [1, 2, 3, "Lunch", 5, 6],
+        B: [4, 1, 2, "Lunch", 7, 5],
+        C: [3, 4, 1, "Lunch", 6, 7],
+        D: [2, 3, 4, "Lunch", 5, 6],
+        E: [1, 2, 3, "Lunch", 7, 5],
+        F: [4, 1, 2, "Lunch", 6, 7],
+        G: [3, 4, 7, "Lunch", 5, 6]
+    }
+
+    
 
     $('#submitDay').on('click', function () {
         const userInput = $('#dayInput').val().trim().toUpperCase();
@@ -22,19 +34,19 @@ $(document).ready(function () {
                 url: "https://api.npoint.io/88e5f5cd29d3eaedfa72",
                 success: function (data) {
                     let schedule = data.schedule.filter(classItem =>
-                        classItem.days.includes(userInput))
-
+                    classItem.days.includes(userInput))
+                    $('#scheduleList').empty()
                     let htmlString = "<tr>"
-
                     schedule.forEach(classItem => {
                         htmlString += `<td> ${classItem.period}</td>
-                                        <td> "Coming back to this" </td>
+                                        <td> "COMING BACK TO THIS"</td>
                                         <td> ${classItem.class} </td>
                                         <td> ${classItem.teacher} </td>
-                                        <td> ${classItem.room} </td>
-                                        </tr>`
-                        console.log(htmlString)
+                                        <td> ${classItem.room} </td>`
+                        htmlString += '</tr>'
                     })
+
+                    $('#scheduleList').append(htmlString)
 
                 }
             });
